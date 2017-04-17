637f804dc732afef7c52f135a9dc1e17a58187fb
angrycola <youagain@yandex.ru>
2017-04-17 08:59:47 +0300

    JWT on Signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,12 @@
+import jwt from 'jwt-simple';
+import secret from '../lib/secret/passport';
+
 import User from '../models/user';
+
+const tokenForUser = user => {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp, email: user.email }, secret.key);
+};
 
 export const signup = (req, res, next) => {
   const { email, password } = req.body;
@@ -9,10 +17,9 @@ export const signup = (req, res, next) => {
     if (existingUser) res.status(422).send({ error: 'This email is already in use' });
 
     const user = new User({ email, password });
-
     user.save((err) => {
       if (err) return next(err);
-      res.json({ user });
+      res.json({ token: tokenForUser(user) });
     });
   });
 };
--- models/user.js
+++ models/user.js
@@ -1,8 +1,22 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
 
 const UserSchema = new Schema ({
   email:    { type: String, required: true, minlength: 5, trim: true, unique: true, lowercase: true, index: true },
   password: { type: String, required: true, minlength: 3, trim: true }
 });
+
+
+UserSchema.pre('save', function(next) {
+  const user = this;
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 
 export default mongoose.model('User', UserSchema);
