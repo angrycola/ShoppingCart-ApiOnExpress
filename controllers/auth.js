46691e8e5a0542454dcc321469000eba7843a7b7
angrycola <youagain@yandex.ru>
2017-04-17 08:46:10 +0300

    POST Signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,18 @@
+import User from '../models/user';
+
 export const signup = (req, res, next) => {
-  res.send({ success: 'echo'});
+  const { email, password } = req.body;
+  if (!email || !password) return res.status(422).send({ error: 'Email and password must be present'});
+
+  User.findOne({ email }, (err, existingUser) => {
+    if (err) return next(err);
+    if (existingUser) res.status(422).send({ error: 'This email is already in use' });
+
+    const user = new User({ email, password });
+
+    user.save((err) => {
+      if (err) return next(err);
+      res.json({ user });
+    });
+  });
 };
